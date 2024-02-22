e2b4f491452e177b7144aa0d5dd5b32a683d45f1
Phan Anh Tu <okthichiatay77@gmail.com>
2024-02-22 21:09:13 +0700

    update code

--- accounts/views.py
+++ accounts/views.py
@@ -9,88 +9,92 @@ from . import models
 
 
 def login_view(request):
-    form = AuthenticationForm()
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+	form = AuthenticationForm()
+	if request.method == 'POST':
+		form = AuthenticationForm(data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
 
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect(reverse('app:home'))
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				return HttpResponseRedirect(reverse('app:home'))
 
-    return render(request, 'accounts/login.html', {'form': form})
+	return render(request, 'accounts/login.html', {'form': form})
 
 
 def sign_up_view(request):
-    form = forms.CreateNewUser()
-    if request.method == 'POST':
-        form = forms.CreateNewUser(request.POST)
-        if form.is_valid():
-            user = form.save()
-            create_profile = models.UserProfile.objects.create(user=user)
-            create_profile.save()
-            return HttpResponseRedirect(reverse('accounts:login'))
+	form = forms.CreateNewUser()
+	if request.method == 'POST':
+		form = forms.CreateNewUser(request.POST)
+		if form.is_valid():
+			user = form.save()
+			create_profile = models.UserProfile.objects.create(user=user)
+			create_profile.save()
+			return HttpResponseRedirect(reverse('accounts:login'))
 
-    return render(request, 'accounts/sign_up.html', {'form': form})
+	return render(request, 'accounts/sign_up.html', {'form': form})
 
 
 @login_required
 def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect(reverse('app:home'))
+	logout(request)
+	return HttpResponseRedirect(reverse('app:home'))
 
 
 @login_required
 def profile_detail_view(request):
-    form = forms.EditProfile()
-    profile = models.UserProfile.objects.get(user=request.user)
-    if request.method == 'POST':
-        form = forms.EditProfile(request.POST or None, request.FILES, instance=profile)
-        if form.is_valid():
-            form.save(commit=True)
-            return HttpResponseRedirect(reverse('accounts:profile'))
+	form = forms.EditProfile()
+	profile = models.UserProfile.objects.get(user=request.user)
+	if request.method == 'POST':
+		form = forms.EditProfile(request.POST or None, request.FILES, instance=profile)
+		if form.is_valid():
+			form.save(commit=True)
+			return HttpResponseRedirect(reverse('accounts:profile'))
 
-    context = {
-        'profile': profile,
-        'form': form,
-    }
-    return render(request, 'accounts/profile.html', context=context)
+	context = {
+		'profile': profile,
+		'form': form,
+	}
+	return render(request, 'accounts/profile.html', context=context)
 
 
 @login_required
 def edit_profile_view(request):
-    form = forms.EditProfile()
-    profile = models.UserProfile.objects.get(user=request.user)
-    if request.method == 'POST':
-        form = forms.EditProfile(request.POST or None, request.FILES, instance=profile)
-        if form.is_valid():
-            form.save(commit=True)
-            return HttpResponseRedirect(reverse('accounts:profile'))
+	form = forms.EditProfile()
+	profile = models.UserProfile.objects.get(user=request.user)
+	if request.method == 'POST':
+		form = forms.EditProfile(request.POST or None, request.FILES, instance=profile)
+		if form.is_valid():
+			form.save(commit=True)
+			return HttpResponseRedirect(reverse('accounts:profile'))
 
-    context = {
-        'form': form
-    }
-    return render(request, 'accounts/edit_profile.html', context=context)
+	context = {
+		'form': form
+	}
+	return render(request, 'accounts/edit_profile.html', context=context)
 
 
 @login_required
 def change_password_view(request, pk):
-    mess = ""
-    if request.method == 'POST' and 'your_news_password1' in request.POST:
-        new_password = request.POST['your_news_password1']
-        new_password_conf = request.POST['your_news_password2']
-        if new_password != new_password_conf:
-            mess = "Mật khẩu mới bạn nhập không trùng nhau, vui lòng thử lại!"
-        else:
-            cur_user = models.User.objects.get(pk=pk)
-            cur_user.set_password(new_password)
-            cur_user.save()
-            mess = "Bạn đã thay đổi mật khẩu thành công!"
-            return HttpResponseRedirect(reverse('app:home'))
-    context = {
-        'mess': mess
-    }
-    return render(request, 'accounts/change_password.html', context=context)
+	mess = ""
+	if request.method == 'POST' and 'your_news_password1' in request.POST:
+		cur_user = models.User.objects.get(pk=pk)
+		old_pass = request.POST['old_password']
+		new_password = request.POST['your_news_password1']
+		new_password_conf = request.POST['your_news_password2']
+		print("--check", cur_user.check_password(old_pass))
+		if not cur_user.check_password(old_pass):
+			mess = 'Bạn nhập mật khẩu cũ không đúng, vui lòng nhập lại!'
+		elif new_password != new_password_conf:
+			mess = "Mật khẩu mới bạn nhập không trùng nhau, vui lòng thử lại!!"
+		else:
+			cur_user.set_password(new_password)
+			cur_user.save()
+			mess = "Bạn đã thay đổi mật khẩu thành công!"
+			return HttpResponseRedirect(reverse('app:home'))
+	context = {
+		'mess': mess
+	}
+	return render(request, 'accounts/change_password.html', context=context)
--- firstapp/views.py
+++ firstapp/views.py
@@ -2,32 +2,36 @@ from django.shortcuts import render
 
 from .models import CVE, Affected, References, Metric
 
+
 def index_view(request):
-    list_cve = CVE.objects.all()[:3]
-    context = {
-        'list_test': [1, 2, 3],
-        'list_news': list_cve,
-    }
-    return render(request, 'home.html', context=context)
+	list_cve = CVE.objects.all()[:3]
+	if request.method == 'POST':
+		id_cve = request.POST['id_cve']
+		list_cve = CVE.objects.filter(title__contains=id_cve)
+
+	context = {
+		'list_test': [1, 2, 3],
+		'list_news': list_cve,
+	}
+	return render(request, 'home.html', context=context)
 
 
 def list_cves_view(request):
+	if request.method == 'POST':
+		id_cve = request.POST['search_focus']
+		list_cve = CVE.objects.filter(title__contains=id_cve)
+	else:
+		list_cve = CVE.objects.all()
+	context = {
+		# 'list_cve': [1, 2, 3, 4],
+		'list_cve': list_cve
+	}
+	return render(request, 'firstapp/list_cves.html', context=context)
 
-    if request.method == 'POST':
-        id_cve = request.POST['search_focus']
-        list_cve = CVE.objects.filter(title__contains=id_cve)
-        print(list_cve)
-    else:
-        list_cve = CVE.objects.all()
-    context = {
-        # 'list_cve': [1, 2, 3, 4],
-        'list_cve': list_cve
-    }
-    return render(request, 'firstapp/list_cves.html', context=context)
 
 def detail_cves_view(request, pk):
-    detail_cve = CVE.objects.get(pk=pk)
-    context = {
-        'detail_cve': detail_cve
-    }
-    return render(request, 'firstapp/detail_cve.html', context=context)
+	detail_cve = CVE.objects.get(pk=pk)
+	context = {
+		'detail_cve': detail_cve
+	}
+	return render(request, 'firstapp/detail_cve.html', context=context)
